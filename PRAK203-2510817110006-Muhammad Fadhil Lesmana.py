54ef1f7bc2ec5f408ca3e4eca46c3874cda36f66
MuhammadFadhilLesmana <fadhillesmana45@gmail.com>
2025-10-22 00:13:50 +0800

    PRAK203-2510817110006-Muhammad Fadhil Lesmana.py

---
+++ PRAK203-2510817110006-Muhammad Fadhil Lesmana.py
@@ -0,0 +1,9 @@
+# Input
+nilai1 = float(input("Masukkan Nilai Pertama : "))
+nilai2 = float(input("Masukkan Nilai Kedua : "))
+
+# Proses
+hasil = nilai1 + nilai2
+
+# Output
+print(f'Hasil dari penjumlahan nilai pertama "{nilai1}" dan nilai kedua "{nilai2}" adalah "{hasil:.2f}"')
