beff05389c3d379689a0c4b9e58bce9e8c7e52bc
MuhammadFadhilLesmana <fadhillesmana45@gmail.com>
2025-10-22 00:10:07 +0800

    PRAK201-2510817110006-Muhammad Fadhil Lesmana.py

---
+++ PRAK201-2510817110006-Muhammad Fadhil Lesmana.py
@@ -0,0 +1,18 @@
+# Input biodata
+Nama = input("Nama: ")
+NIM = input("NIM: ")
+Kelas = input("Kelas Paralel: ")
+ttl = input("Tempat/Tanggal Lahir: ")
+Alamat = input("Alamat: ")
+Hobby = input("Hobby: ")
+No_HP = input("No. HP: ")
+
+# Output biodata
+print("\nOutput")
+print("Nama:", Nama)
+print("NIM:", NIM)
+print("Kelas Paralel:", Kelas)
+print("Tempat/Tanggal Lahir:", ttl)
+print("Alamat:", Alamat)
+print("Hobby:", Hobby)
+print("No. HP:", No_HP)
