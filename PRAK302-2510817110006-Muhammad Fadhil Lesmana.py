6067a5513aed68d62478590c8c7b96f977f75ac5
MuhammadFadhilLesmana <fadhillesmana45@gmail.com>
2025-10-27 09:09:57 +0800

    PRAK302-2510817110006-Muhammad Fadhil Lesmana.py

---
+++ PRAK302-2510817110006-Muhammad Fadhil Lesmana.py
@@ -0,0 +1,15 @@
+try:
+    while True:
+        nilai = int(input())
+        if nilai >= 80:
+            print("A")
+        elif nilai >= 70:
+            print("B")
+        elif nilai >= 60:
+            print("C")
+        elif nilai >= 50:
+            print("D")
+        else:
+            print("E")
+except EOFError:
+    pass
