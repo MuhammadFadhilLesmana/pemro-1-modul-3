c42b3258b7938688afe34373f6695f480e79eddc
MuhammadFadhilLesmana <fadhillesmana45@gmail.com>
2025-10-27 09:11:28 +0800

    PRAK303-2510817110006-Muhammad Fadhil Lesmana.py

---
+++ PRAK303-2510817110006-Muhammad Fadhil Lesmana.py
@@ -0,0 +1,11 @@
+try:
+    while True:
+        N = int(input())
+        if N > 0:
+            print("positif")
+        elif N < 0:
+            print("negatif")
+        else:
+            print("nol")
+except EOFError:
+    pass
